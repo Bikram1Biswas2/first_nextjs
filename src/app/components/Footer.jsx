edb0197727d37e618b521edff44d11c7d6491fe2
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-29 00:52:56 +0530

    create a basic nextjs project and craete navbar and footer file

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+    return (
+        <div className="text-center">
+            <h2>Footer</h2>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul className="flex justify-evenly items-center">
+                <li>Home</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;

c6d66aefaa437f52ee6d0afe0953829cb624d3c6
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-31 17:22:31 +0530

    Navigate the pages using Link tag

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,8 +10,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-      },
+      }
     },
   },
-  plugins: [],
+  plugins: [require('daisyui'),],
 };
